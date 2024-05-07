2b2601caadeeed5c04c3677516415453d55f9079
Marcelo Gadelha <marcelo.gadelha@mds.gov.br>
2024-05-07 12:32:20 -0300

    ajustes nos arquivos

--- www/index.php
+++ www/index.php
@@ -1,5 +1,6 @@
 <?php
 
+    echo "Se mostrar os estados abaixo funcionou:<p>";
     $conn = pg_connect("host=pgcensosuas port=5432 dbname=dbcensosuas user=postgres password=senha123");
     $result = pg_query($conn, "select * from ibge.estados");
     echo "<pre>";
