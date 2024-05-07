5cc3888dc3c97de2ae62c30818ebf1b755566931
Marcelo Gadelha <marcelo.gadelha@mds.gov.br>
2024-05-07 10:38:35 -0300

    php conectando ao postgres

--- c-web/www/index.php
+++ c-web/www/index.php
@@ -1,5 +1,7 @@
 <?php
 
-    $conn = pg_connect("host=localhost port=5432 dbname=dbcensosuas user=postgres password=senha123");
+    $conn = pg_connect("host=dbcensosuas port=5432 dbname=dbcensosuas user=postgres password=senha123");
     $result = pg_query($conn, "select * from ibge.estados");
+    echo "<pre>";
     var_dump(pg_fetch_all($result));
+    echo "</pre>";
